385d89528e0abe1cdf1101716e07457b820ba4f5
Saoa35 <saoamah@gmail.com>
2022-05-24 21:01:11 +0200

    + Home @ About routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,36 @@
 import './App.css';
 import React from 'react';
 
+class Home extends React.Component {
+
+  render() {
+
+    return(
+      <div>
+        <h3>
+          Home Page
+        </h3>
+      </div>
+    )
+  }
+}
+
+class About extends React.Component {
+
+  render() {
+
+    return(
+      <div>
+        <h3>
+          About Page
+        </h3>
+      </div>
+    )
+  }
+}
+
+
+
 function App() {
   return (
     <div className="App">
